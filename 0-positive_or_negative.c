760728488c05415c570444cb2f5650bf4cb4ae42
Your Name <you@example.com>
2023-07-14 08:56:10 -0700

    The 0-positive_or_negative.c commit

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf(n, ":d%\n is positive");
+}
+else if (n == 0)
+{
+printf(n, ":d%\n is zero");
+}
+else
+}
+printf(n, ":d%\n is negative");
+}
+return (0);
+}
