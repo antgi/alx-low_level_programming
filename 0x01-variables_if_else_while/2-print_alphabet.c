1c1ad0be666950bca32a38a2d961fc02b4872d57
Your Name <you@example.com>
2023-07-14 13:29:58 -0700

    The 2-print_alphabet.c commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Tis code prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
