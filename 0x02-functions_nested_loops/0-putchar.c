7c4bae09c6d1f779befb5c7c3d4c76577bc31503
Your Name <you@example.com>
2023-07-18 09:25:51 -0700

    The 0-putchar.c commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
-#include<main.h>
+#include "main.h"
 /**
- * main - outputs _putchar followed by a new line
- *
- * Return: Always 0 (Success)
- */
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
 int main(void)
 {
-myput();
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
-}
-void myput(void)
-{
-putchar("_putchar");
 }
