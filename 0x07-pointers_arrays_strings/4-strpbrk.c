2cbc8592bdbcd698effeb593111071baa5f5ebd3
antgi <anlebu05@gmail.com>
2023-07-31 23:05:32 +0300

    Committing 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,7 +10,8 @@ char *_strpbrk(char *s, char *accept)
 int k;
 while (*s)
 {
-for (k = 0; accept[k]; k++)												{
+for (k = 0; accept[k]; k++)
+{
 if (*s == accept[k])							
 return (s);						
 }
