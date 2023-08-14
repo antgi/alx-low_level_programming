e0b110e08138fb1b46834cda9e8e3c4caddd4c60
antgi <anlebu05@gmail.com>
2023-08-14 11:37:21 -0700

    Committing 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - It will print the name of the file
+ *
+ * Return: It will be always 0 (Success)
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
