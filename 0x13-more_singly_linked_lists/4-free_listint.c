afe4187e823a805add2767909d3e2afa4c2c2b40
antgi <anlebu05@gmail.com>
2023-08-30 07:57:45 -0700

    Committing 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint - It will free a linked list
+ * @head: Represents the listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+}
