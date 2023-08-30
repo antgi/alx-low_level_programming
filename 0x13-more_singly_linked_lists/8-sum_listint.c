26997c15ed3ed37bc69d713c5b45ececfb341469
antgi <anlebu05@gmail.com>
2023-08-30 11:28:22 -0700

    Committting 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_listint - This will calculate sum of all the data in a listint_t list
+ * @head: Describes the first node in the linked list
+ *
+ * Return: Will return the resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+listint_t *temp = head;
+while (temp)
+{
+sum += temp->n;
+temp = temp->next;
+}
+return (sum);
+}
