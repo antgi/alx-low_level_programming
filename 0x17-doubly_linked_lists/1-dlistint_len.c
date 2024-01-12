10fef66a8ad7779d44ccebefd2c93429d898f450
antgi <anlebu05@gmail.com>
2024-01-12 15:57:18 -0800

    Committing Dlistint

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * dlistint_len - Will return the number of elements in
+ * a double linked list
+ *
+ * @h: Descirbes head of the list
+ * Return: Holds the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+int count;
+count = 0;
+if (h == NULL)
+return (count);
+while (h->prev != NULL)
+h = h->prev;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
